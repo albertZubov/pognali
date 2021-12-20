2d71ac3b88619e214abcfeb4b1175f7bad71ec4d
Albert <zubov.warrior@gmail.com>
2021-12-20 10:31:54 +0300

    Завершение верстки и стилизации главной страницы

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -4,7 +4,13 @@ const Footer = () => {
 	return (
 		<footer className='footer container'>
 			<a className='footer__logo'>
-				<img src='' alt='' />
+				<img
+					className='footer__logo-image'
+					src='img/logo-black.webp'
+					width='100'
+					height='19'
+					alt='Погнали?'
+				/>
 			</a>
 			<div className='footer__social footer-social'>
 				<ul className='footer-social__list'>
@@ -13,21 +19,7 @@ const Footer = () => {
 							className='footer-social__link footer-social__link--facebook'
 							href='https://www.facebook.com/htmlacademy'
 						>
-							<span className='visually-hidden'>Мы в Фейсбуке</span>
-							<svg
-								fill='#192144'
-								className='footer-social__link-image footer-social__link-image--facebook'
-								version='1.1'
-								xmlns='http://www.w3.org/2000/svg'
-								x='0px'
-								y='0px'
-								viewBox='0 0 11 21'
-								xmlSpace='preserve'
-								width='11'
-								height='21'
-							>
-								<use xlinkHref='#icon-facebook'></use>
-							</svg>
+							Мы в Фейсбуке
 						</a>
 					</li>
 					<li className='footer-social__item'>
@@ -35,21 +27,7 @@ const Footer = () => {
 							className='footer-social__link footer-social__link--vkontakte'
 							href='https://vk.com/htmlacademy'
 						>
-							<span className='visually-hidden'>Мы в Вконтакте</span>
-							<svg
-								fill='#192144'
-								className='footer-social__link-image footer-social__link-image--vkontakte'
-								version='1.1'
-								xmlns='http://www.w3.org/2000/svg'
-								x='0px'
-								y='0px'
-								viewBox='0 0 30 15.8'
-								xmlSpace='preserve'
-								width='23'
-								height='12'
-							>
-								<use xlinkHref='#icon-vkontakte'></use>
-							</svg>
+							Мы в Вконтакте
 						</a>
 					</li>
 					<li className='footer-social__item'>
@@ -57,21 +35,7 @@ const Footer = () => {
 							className='footer-social__link footer-social__link--instagram'
 							href='https://www.instagram.com/htmlacademy'
 						>
-							<span className='visually-hidden'>Мы в Инстаграме</span>
-							<svg
-								fill='#192144'
-								className='footer-social__link-image footer-social__link-image--instagram'
-								version='1.1'
-								xmlns='http://www.w3.org/2000/svg'
-								x='0px'
-								y='0px'
-								viewBox='0 0 23 23'
-								xmlSpace='preserve'
-								width='18'
-								height='18'
-							>
-								<use xlinkHref='#icon-instagram'></use>
-							</svg>
+							Мы в Инстаграме
 						</a>
 					</li>
 				</ul>
@@ -83,7 +47,7 @@ const Footer = () => {
 				<b className='footer__copyright-text'>Разработано:</b>
 				<img
 					className='footer__copyright-image'
-					src='img/htmlacademy.svg'
+					src='img/icons/htmlacademy.svg'
 					width='16'
 					height='16'
 					alt='html academy'
